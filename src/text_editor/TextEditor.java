a1d64f2088e38d00a2dda3df57470f777707d452
pi22by7 <70760826+pi22by7@users.noreply.github.com>
2021-12-19 19:23:56 +0530

    Right Click Context menu is in + some more bindings

--- src/text_editor/TextEditor.java
+++ src/text_editor/TextEditor.java
@@ -1,11 +1,12 @@
 package text_editor;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
-import javax.swing.undo.*;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.io.*;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -16,6 +17,7 @@ public final class TextEditor extends JFrame implements ActionListener {
     private static JTextArea area;
     private static JCheckBox item_wrap;
     private static UndoManager manager;
+    private JPopupMenu rcontext;
 
     public TextEditor() {
         run();
@@ -31,9 +33,8 @@ public final class TextEditor extends JFrame implements ActionListener {
         }
         area = new JTextArea();
         frame.add(area);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setSize(640, 480);
-        frame.setVisible(true);
 
         JMenuBar menu_main = new JMenuBar();
 
@@ -46,6 +47,7 @@ public final class TextEditor extends JFrame implements ActionListener {
 
         JMenuItem item_new = new JMenuItem("New");
         item_new.setMnemonic(KeyEvent.VK_N);
+        JMenuItem item_new_window = new JMenuItem("New Window");
         JMenuItem item_open = new JMenuItem("Open");
         item_open.setMnemonic(KeyEvent.VK_O);
         JMenuItem item_save = new JMenuItem("Save");
@@ -55,28 +57,42 @@ public final class TextEditor extends JFrame implements ActionListener {
         item_wrap.setMnemonic(KeyEvent.VK_T);
         JMenuItem item_undo = new JMenuItem("Undo");
         JMenuItem item_redo = new JMenuItem("Redo");
+        JMenuItem item_cut = new JMenuItem("Cut");
+        item_cut.addActionListener(this);
+        JMenuItem item_copy = new JMenuItem("Copy");
+        item_copy.addActionListener(this);
+        JMenuItem item_paste = new JMenuItem("Paste");
+        item_paste.addActionListener(this);
 
         item_new.addActionListener(this);
+        item_new_window.addActionListener(this);
         item_open.addActionListener(this);
         item_save.addActionListener(this);
         item_quit.addActionListener(this);
         item_wrap.addActionListener(this);
         item_undo.addActionListener(this);
         item_redo.addActionListener(this);
-
-        menu_main.add(submenu_file);
-        menu_main.add(submenu_edit);
-        menu_main.add(submenu_format);
+        item_copy.addActionListener(this);
+        item_cut.addActionListener(this);
+        item_paste.addActionListener(this);
 
         submenu_file.add(item_new);
+        submenu_file.add(item_new_window);
         submenu_file.add(item_open);
         submenu_file.add(item_save);
         submenu_file.add(item_quit);
 
         submenu_edit.add(item_undo);
         submenu_edit.add(item_redo);
+        submenu_edit.add(item_cut);
+        submenu_edit.add(item_copy);
+        submenu_edit.add(item_paste);
 
         submenu_format.add(item_wrap);
+
+        menu_main.add(submenu_file);
+        menu_main.add(submenu_edit);
+        menu_main.add(submenu_format);
 
         frame.setJMenuBar(menu_main);
 
@@ -92,6 +108,9 @@ public final class TextEditor extends JFrame implements ActionListener {
         KeyStroke keyStrokeNew = KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK);
         item_new.setAccelerator(keyStrokeNew);
 
+        KeyStroke keyStrokeNewWin = KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK | KeyEvent.SHIFT_DOWN_MASK);
+        item_new_window.setAccelerator(keyStrokeNewWin);
+
         KeyStroke keyStrokeSave = KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK);
         item_save.setAccelerator(keyStrokeSave);
 
@@ -100,6 +119,49 @@ public final class TextEditor extends JFrame implements ActionListener {
 
         KeyStroke keyStrokeRedo = KeyStroke.getKeyStroke(KeyEvent.VK_Y, KeyEvent.CTRL_DOWN_MASK);
         item_redo.setAccelerator(keyStrokeRedo);
+
+        KeyStroke keyStrokeCopy = KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK);
+        item_copy.setAccelerator(keyStrokeCopy);
+
+        KeyStroke keyStrokeCut = KeyStroke.getKeyStroke(KeyEvent.VK_X, KeyEvent.CTRL_DOWN_MASK);
+        item_cut.setAccelerator(keyStrokeCut);
+
+        KeyStroke keyStrokePaste = KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.CTRL_DOWN_MASK);
+        item_paste.setAccelerator(keyStrokePaste);
+
+
+        rcontext = new JPopupMenu();
+
+        JMenuItem item_cUndo = new JMenuItem("Undo");
+        item_cUndo.addActionListener(this);
+        rcontext.add(item_cUndo);
+
+        rcontext.addSeparator();
+
+        JMenuItem item_cCut = new JMenuItem("Cut");
+        item_cCut.addActionListener(this);
+        rcontext.add(item_cCut);
+
+        JMenuItem item_cCopy = new JMenuItem("Copy");
+        item_cCopy.addActionListener(this);
+        rcontext.add(item_cCopy);
+
+        JMenuItem item_cPaste = new JMenuItem("Paste");
+        item_cPaste.addActionListener(this);
+        rcontext.add(item_cPaste);
+
+        rcontext.addSeparator();
+
+        JMenuItem item_selAll = new JMenuItem("Select All");
+        item_selAll.addActionListener(this);
+        rcontext.add(item_selAll);
+
+        MouseListener popupListener = new PopupListener();
+        area.addMouseListener(popupListener);
+
+        area.add(rcontext);
+
+        frame.setVisible(true);
     }
 
     private void undo() {
@@ -110,6 +172,7 @@ public final class TextEditor extends JFrame implements ActionListener {
         }
         catch (CannotUndoException ignore) {}
     }
+
     private void redo() {
         try {
             if (manager.canRedo()) {
@@ -117,6 +180,23 @@ public final class TextEditor extends JFrame implements ActionListener {
             }
         }
         catch (CannotRedoException ignore) {}
+    }
+
+    class PopupListener extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            maybeShowPopup(e);
+        }
+
+        public void mouseReleased(MouseEvent e) {
+            maybeShowPopup(e);
+        }
+
+        private void maybeShowPopup(MouseEvent e) {
+            if (e.isPopupTrigger()) {
+                rcontext.show(e.getComponent(),
+                        e.getX(), e.getY());
+            }
+        }
     }
 
     @Override
@@ -184,6 +264,7 @@ public final class TextEditor extends JFrame implements ActionListener {
                 area.setText("");
                 frame.setTitle("πpad");
             }
+            case "New Window" -> new TextEditor();
             case "Text Wrap" -> {
                 boolean b = item_wrap.isSelected();
                 item_wrap.setSelected(b);
@@ -198,8 +279,12 @@ public final class TextEditor extends JFrame implements ActionListener {
                     area.setBorder(BorderFactory.createEmptyBorder(0,0,0,0));
                 }
             }
+            case "Cut" -> area.cut();
+            case "Copy" -> area.copy();
+            case "Paste" -> area.paste();
             case "Undo" -> undo();
             case "Redo" -> redo();
+            case "Select All" -> area.selectAll();
             case "Quit" -> System.exit(0);
         }
     }
